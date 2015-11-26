38accf44d1e8ddd66b1a4c3290a3476b2850b24a
Mike Ferrara <mferrara@gmail.com>
2015-11-25 22:54:10 -0800

    Add flash messages view to layouts

--- resources/views/layouts/no-sidebar.blade.php
+++ resources/views/layouts/no-sidebar.blade.php
@@ -4,6 +4,11 @@
 
 <div class="container-fluid">
     <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            @include('flash::message')
+        </div>
+    </div>
+    <div class="row">
         <div class="col-md-12">
             @yield('content')
         </div>
