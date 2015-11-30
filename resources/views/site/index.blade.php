fb24810ccba6df64fbe80d21d59c6e2d00b765cd
Mike Ferrara <mferrara@gmail.com>
2015-11-29 18:18:02 -0800

    Super basic site template

---
+++ resources/views/site/index.blade.php
@@ -0,0 +1,16 @@
+@extends('site.layouts.default')
+
+@section('title') Home - {!! $domain->name !!} @endsection
+
+@section('content')
+    <h1>Hello! Welcome to {!! $domain->name !!}!</h1>
+
+    <h3>Pages</h3>
+    <ul>
+        @foreach($domain->pages()->active()->get() as $page)
+            <li>
+                <a href="{!! $page->present()->url() !!}">{!! $page->present()->pageTitle() !!}</a>
+            </li>
+        @endforeach
+    </ul>
+@endsection
---
+++ resources/views/site/page.blade.php
@@ -0,0 +1,17 @@
+@extends('site.layouts.default')
+
+@section('title') {!! $page->present()->pageTitle() !!} - {!! $page->domain->name !!} @endsection
+
+@section('content')
+
+    <h1>{!! $page->present()->pageTitle() !!}</h1>
+
+    <p>
+        <a href="/">Back home</a>
+    </p>
+
+    <p>
+        {!! $page->present()->pageBody() !!}
+    </p>
+
+@endsection
