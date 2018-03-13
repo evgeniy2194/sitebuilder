55436084626a430080b45039db86a55285e9a732
Mike Ferrara <mferrara@gmail.com>
2018-03-13 01:14:43 -0700

    Add link cloud links to template

--- config/sitebuilder.php
+++ config/sitebuilder.php
@@ -4,6 +4,7 @@ return [
     'remove_links_from_these_domains'   => [
         'walmart.com',
         'amazon.com',
-    ]
+    ],
+    'linkcloud_api_token'   => env('LINKCLOUD_API_TOKEN')
 ];
 
--- resources/views/site/templates/basic/layout.blade.php
+++ resources/views/site/templates/basic/layout.blade.php
@@ -24,6 +24,31 @@
         </div>
     </div>
 
+    <div class="row">
+        <div class="col-md-12">
+            <p class="text-center">
+                @php
+                    $lc_api_token = config('sitebuilder.linkcloud_api_token');
+                    $lc_user_agent = null;
+                    if(isset($_SERVER['HTTP_USER_AGENT']))
+                        $lc_user_agent = $_SERVER['HTTP_USER_AGENT'];
+
+                    $lc_ip_address = null;
+                    if(isset($_SERVER['REMOTE_ADDR']))
+                        $lc_ip_address = $_SERVER['REMOTE_ADDR'];
+
+                    $links = '';
+                    if( ! is_null($lc_user_agent) && ! is_null($lc_ip_address))
+                    {
+                        $request_url = 'https://linkcloud.net/api/v1/links?api_token='.$lc_api_token.'&ip='.urlencode($lc_ip_address).'&ua='.urlencode($lc_user_agent);
+                        $links = file_get_contents($request_url, false);
+                    }
+                    echo $links;
+                @endphp
+            </p>
+        </div>
+    </div>
+
 </div>
 
 </body>
